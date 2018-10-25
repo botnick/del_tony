a4b8c9bbb233e38db2504a411ed6fbfe4eedd8ff
botnick <botnick.xxx@gmail.com>
2018-10-25 15:23:11 +0700

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -4,14 +4,14 @@
 // @include        https://*.facebook.com/*
 // @require        http://code.jquery.com/jquery-1.7.1.min.js
 // @grant       none
-// @version 3.1
+// @version 4
 // @namespace https:/botnick.me/js
 // @description หลบโทนี่บุหรี่ทำเอง
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var banList = ['Bonne chance', 'ไม่ต้อนรับพวกโลกสวย','โทนี่บุหรี่ทำเอง','Bonnechance','โลชั่นน้ำหอม 290 บาท','โลกสวยห้ามเข้ามาดู','18++ ไม่ต้อนรับพวกโลกสวย','18+++ ไม่ต้อนรับพวกโลกสวย'];
+    var banList = ['Bonne chance', 'ไม่ต้อนรับพวกโลกสวย','โทนี่บุหรี่ทำเอง','Bonnechance','โลชั่นน้ำหอม 290 บาท','โลกสวยห้ามเข้ามาดู','18++ ไม่ต้อนรับพวกโลกสวย','18+++ ไม่ต้อนรับพวกโลกสวย','18+ ไม่ต้อนรับพวกโลกสวยช'];
 setInterval(function() {
   var a = document.querySelectorAll('[role="article"]')
   a.forEach((d) => {
