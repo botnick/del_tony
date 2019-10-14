ebc39cb10e6eda1505d016087c5ef33e92a97a26
botnick <botnick.xxx@gmail.com>
2019-10-14 19:52:32 +0700

    add log

--- script.user.js
+++ script.user.js
@@ -4,20 +4,20 @@
 // @include        https://*.facebook.com/*
 // @require        http://code.jquery.com/jquery-1.7.1.min.js
 // @grant       none
-// @version 5
+// @version 6
 // @namespace https:/botnick.me/js
 // @description หลบโทนี่บุหรี่ทำเอง
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var banList = ['Bonne chance', 'ไม่ต้อนรับพวกโลกสวย','โทนี่บุหรี่ทำเอง','Bonnechance','โลชั่นน้ำหอม 290 บาท','โลกสวยห้ามเข้ามาดู','18++ ไม่ต้อนรับพวกโลกสวย','18+++ ไม่ต้อนรับพวกโลกสวย','18+ ไม่ต้อนรับพวกโลกสวยช','ได้รับการสนับสนุน'];
+    var banList = ['Bonne chance', 'ไม่ต้อนรับพวกโลกสวย','โทนี่บุหรี่ทำเอง','Bonnechance','โลชั่นน้ำหอม 290 บาท','โลกสวยห้ามเข้ามาดู','18++ ไม่ต้อนรับพวกโลกสวย','18+++ ไม่ต้อนรับพวกโลกสวย','18+ ไม่ต้อนรับพวกโลกสวยช','ได้รับการสนับสนุน',"กำลังถ่ายทอดสด",'กำลังเล่น'];
 setInterval(function() {
   var a = document.querySelectorAll('[role="article"]')
   a.forEach((d) => {
     banList.map((text) => {
       if (d.innerText.toLocaleLowerCase().indexOf(text) >= 0) {
-        console.log('removed : '+a);
+        console.log('removed : '+text + "::" + d.innerText.substring(0, 150));
         d.remove()
       }
     })
